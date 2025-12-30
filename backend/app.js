d1737f8ba818690f89e756b5867cb31d6c904b9c
HarshGhoghari318 <harshghoghari77@gmail.com>
2025-12-30 19:16:20 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(cors({  
     origin: process.env.NODE_ENV === 'production' 
-      ? ["https://luxury-shop-459l.vercel.app", "https://your-frontend-domain.vercel.app"]
+      ? ["https://luxury-shop-tawny.vercel.app"]
       : "http://localhost:5173",
     credentials: true,
     methods: "GET, POST, PUT, DELETE",
