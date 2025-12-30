daeaf7ec9368680da4d28af926b73864f56c1cb7
HarshGhoghari318 <harshghoghari77@gmail.com>
2025-12-30 20:07:55 +0530

    .

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,18 +1,19 @@
-
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import Context from "./utils/Context.jsx";
 import UserContext from "./utils/UserContext.jsx"; // Corrected import
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <Context>
-    
-    <UserContext>
-      <App />
-    </UserContext>
-  </Context>
+    <Context>
+      <UserContext>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </UserContext>
+    </Context>
   </BrowserRouter>
 );
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "/", // ✅ REQUIRED for Vercel
   plugins: [react()],
   resolve: {
     alias: {
